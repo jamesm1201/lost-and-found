17251a912a1b6252bc0461cb189a0155540bc114
jamesm1201 <jhmorley@icloud.com>
2021-12-14 13:20:56 +0000

    added error displaying to the layout

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -9,6 +9,17 @@
 <body>
     <h1>@yield('title')</h1>
 
+    @if($errors->any())
+        <div>
+            Errors:
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <div>
         @yield('content')
     </div>
